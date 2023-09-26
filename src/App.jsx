894fd8a83d2f0aac83549f3dd8e49d33c5e9a84d
Gael Rodríguez <gaelrodriguezl474@gmail.com>
2023-09-26 00:04:17 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { MyRoutes } from "./routers/routes";
+import { AuthContextProvider } from "./context/AuthContext";
+function App() {
+  return (
+    <AuthContextProvider>
+      <MyRoutes />
+    </AuthContextProvider>
+  );
+}
+
+export default App;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,59 @@
+import { createContext, useEffect, useState } from "react";
+import { supabase } from "../supabase/supabase.config";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children })=>{
+    const navigate = useNavigate();
+    const [user, setUser] = useState([]);
+
+    async function signInWithGoogle() {
+        try {
+            const { data, error } = await supabase.auth.signInWithOAuth({
+                provider: 'google',
+            });
+
+            if(error) throw new Error("A ocurrido un error durante la autenticación");
+            return data;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function signout() {
+        const { error } = await supabase.auth.signOut()
+        if(error) throw new Error("A ocurrido un error durante el cierre de sesión");
+            return data;
+    }
+
+    useEffect(()=> {
+        const {data:authListener} = supabase.auth.onAuthStateChange(async (event, session) => {
+            console.log("Supabase event: ", event);
+            if(session==null) {
+                navigate("/login", {replace:true});
+            }
+            else {
+                setUser(session?.user.user_metadata)
+                console.log("Data del usuario", session?.user.user_metadata)
+                navigate("/", {replace:true});
+            }
+        })
+
+        return () => {
+            authListener.subscription;
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{signInWithGoogle, signout, user}}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export const UserAuth=()=> {
+    return useContext(AuthContext); 
+}
